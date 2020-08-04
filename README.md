4e351f23bc0ec1eb1520b6bb3c1c64bbe2adbd91
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-08-03 20:18:46 -0400

    Criada estrutura básica do projeto e estilo global

---
+++ README.md
@@ -0,0 +1,8 @@
+# Next Level Week 2.0
+Profy - Plataforma para professores e alunos
+
+# Aula 1
+````shell
+$ yarn create react-app web --template typescript
+
+```
---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './assets/styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, from React</h1>
+      <h3>on NextLevelWeek 2.0!</h3>
+      <button type="button">Click me!</button>
+    </div>
+  );
+}
+
+export default App;
